c93b9778e0463daacf252ac4c79196bd40b17f0e
NiharikaSangolkar <111430886+NiharikaSangolkar@users.noreply.github.com>
2024-05-10 16:50:58 +0530

    Create sumofarray.java

---
+++ sumofarray.java
@@ -0,0 +1,22 @@
+// Write a Java Program to Compute the Sum of Array Elements.
+
+class sumofarray{
+    public static void main(String[] args) {
+       // System.out.println("Try programiz.pro");
+       int arr[]={2,4,9,6,7};
+       int sum=0;
+       for(int i=0;i<arr.length;i++)
+       {
+           
+           sum+=arr[i];
+       }
+       System.out.println("sum of arrays:"+sum);
+    }
+}
+/*
+op:
+sum of arrays:28
+
+
+
+*/
