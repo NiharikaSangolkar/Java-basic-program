2515207fa672aa58137e9e32c4fbe8ec18cd8023
Niharika Sangolkar <111430886+NiharikaSangolkar@users.noreply.github.com>
2024-10-13 18:32:09 +0530

    Linkedlist1.java

---
+++ Linkedlist1.java
@@ -0,0 +1,18 @@
+/*Write a Java program to append the specified element to the end of a linked list.
+*/
+
+import java.util.*;
+class appendls{
+    public static void main(String[]args)
+    {
+        LinkedList<String>ls=new LinkedList<String>();
+        ls.add("green");
+        ls.add("blue");
+        ls.add("pink");
+        System.out.println("list are:"+ls);
+    }
+}
+
+/* op
+list are:[green, blue, pink]
+*/
