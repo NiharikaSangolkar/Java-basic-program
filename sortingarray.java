7501283c252fb6736c828580831c59f1872a0742
Niharika Sangolkar <111430886+NiharikaSangolkar@users.noreply.github.com>
2025-11-13 17:16:24 +0530

    sortingarray.java

---
+++ sortingarray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ArraySort{
+    public static void main(String[]args)
+    {
+        int arr[]={5,7,1,4,6,-3,-34,78};
+         System.out.println(" before sorting array:"+arr);
+        Arrays.sort(arr);
+        System.out.println(" after sorting array:"+arr.toString());
+    }
+}
